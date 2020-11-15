9165d51d286aacc09642b01c1d8495f172330f5d
Rovine1999 <rovinewanjala99@gmail.com>
2020-11-15 21:47:51 +0300

    added line profile.services.ts

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
+import { Map } from 'rxjs/operator';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ private username: string;
 private clientid = 'afd0ed65cc4d18a33699';
 private clientsecret = '47e571b721fc454e4e4e057c3e9e09c406171fee';
 
-  constructor(private http: Http) { 
+  constructor(private http: HttpClient, operator: Map) { 
     console.log("service is not ready!");
     this.username = 'Rovine1999';
   }
